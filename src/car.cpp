216d65d9b21897a8aa41541a03c566be3de63299
mikedlr87 <mikedlr1115@gmail.com>
2024-11-17 00:26:32 -0400

    feat:se incluyeron funciones de prueba y se inicio la estructura de los clientes

--- src/car.cpp
+++ src/car.cpp
@@ -21,12 +21,20 @@ struct Vehiculo{
    }; 
 //hacer la estructura del cliente
 struct Cliente{
-    int cedula;
+    int ced_cliente;
     string nombre;
     string apellido;
-    string email;
+    string e_mail;
+    int cantidad_vehiculos_rentados;
+    string direccion;
+    string activo;
+
     
    }; 
+
+   void guardarCambios(){
+    manejoVehiculo.guardarCambioVehiculo();
+   }
 
 const int maxVehiculos = 100;
 
@@ -81,14 +89,112 @@ void mostrarVehiculo() const {
         <<veh.motor<<", Precio renta: "<<veh.precio_renta <<" , Fecha de entrega:" <<veh.fecha_de_entrega<<"\n";
     }
 }
+//funciones para mostrar consultas
+  void mostrarPrecioVehiculo() const {
+    for (int i=0; i<numVehiculos; ++i){
+        const Vehiculo& veh = vehiculos[i];
+        cout<< i + 1 <<", marca: "<<veh.marca<< ", precio renta: "<<veh.precio_renta<< ", placa: "<<veh.placa<<"\n";
+    }
+  }
+void guardarCambiosVehiculos() const{
+      ofstream file("../bin/vehicle.csv");
+    file <<"modelo,marca,placa,color,anio,kilometraje,rentado,motor,precio_renta,ced_cliente,fecha_entrega\n";
+    for (int i=0; i<numVehiculos; ++i){
+        const Vehiculo& veh = vehiculos[i];
+        file << veh.modelo <<','<< veh.marca
+        <<','<<veh.placa<<','<<veh.color<<','<<veh.anio<<','
+        <<veh.kilometraje<<','<<(veh.rentado ? "true":"false")<<','
+        <<veh.motor<<','<<veh.precio_renta <<',' <<veh.fecha_de_entrega<<'\n';
+    }
+}
+
+
+
+
+
+
 private:
     Vehiculo vehiculos[maxVehiculos];
     int numVehiculos;
  };
+
+const int maxClientes = 100;
+class manejoCliente{
+public:
+
+manejoClientes():numClientes (0){
+cargarClientes();
+
+void cargarClientes(){
+ifstream file("../bin/client.csv");
+string line;
+
+(getline(file,line));
+
+while (getline(file,line) && numClientes < maxClientes){
+istringstream row(line);
+Cliente& cliente = clientes[numClientes];
+string temp;
+
+
+getline(row, temp, ',');
+cliente.ced_cliente = (!temp.empty() ? stoi(temp) : 0);
+getline(row, cliente.nombre, ',');
+getline(row, cliente.apellido, ',');
+getline(row, cliente.e_mail, ',');
+getline(row, temp, ',');
+cliente.cliente.cantidad_vehiculos_rentados = (!temp.empty() ? stoi(temp) : 0);
+getline(row, cliente.direccion, ',');
+getline(row, cliente.activo, ',');
+
+}
+
+
+
+
+
+
+};
+
+
 int main() {
-    manejoVehiculo gestion;
-    gestion.mostrarVehiculo();
+        manejoVehiculo gestion;
+    //    manejoRepuestos gestonRepuestos
+
+        int eleccion;
+
+            do {
+                cout<< "\ningrese 1 si quiere mostrar los vehiculos: \ningrese 2 si mostrar los clientes\ningrese 3 si ver los repuestos ";
+                cin>> eleccion;
+                if (eleccion == 1){
+                    gestion.mostrarVehiculo();
+                } else {
+                    cout << "\nNo lo es";
+                }
+
+                if (eleccion == 2){
+                    // gestion.mostrarClientes();
+                } else {
+                    cout << "\nNo lo es";
+                }
+
+                if (eleccion == 3){
+                    // gestionRepuestos.mostrarRepuestos();
+                } else {
+                    cout << "\nNo lo es";
+                }
+                
+         /*       
+                    do {
+                }  while ();
+
+*/
+
+            }   while (eleccion != 0);
 
 
+
+    
 return 0;
    }
+ 
